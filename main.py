8101ab57ed5dfb7ccb32afecd3dc9e8cd0a6c017
Arcadiyyyyyyyy <Arcadiyyyyyyyy@gmail.com>
2023-04-27 17:33:42 +0100

    cleanup: static.py created
    Removed redundant global variable statements, moved static variables, improved visual state of modules imports

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from dotenv.main import load_dotenv
 import telebot
 import openai
-from dotenv.main import load_dotenv
+import static
 import json
 import os
 
@@ -31,22 +32,10 @@ bot = telebot.TeleBot(os.getenv("TELEGRAM_API_KEY"))
 # получаем чат_айди админа, которому в лс будут приходить логи
 admin_chat_id = int(os.getenv("ADMIN_CHAT_ID"))
 
-# prompt = "You are Marv - a sarcastic reluctant assistant."
-prompt = "You are a helpful assistant."
-
-session_tokens = 0
-request_number = 0
-price_1k = 0.002
-
-price_cents = price_1k / 10
-
 
 # Define the message handler for incoming messages
 @bot.message_handler(func=lambda message: True)
 def handle_message(message):
-    global session_tokens
-    global request_number
-    global prompt
     global data
 
     # Send the user's message to OpenAI API and get the response
@@ -54,15 +43,14 @@ def handle_message(message):
         model="gpt-3.5-turbo",
         max_tokens=3000,
         messages=[
-            {"role": "system", "content": prompt},
+            {"role": "system", "content": static.prompt},
             {"role": "user", "content": message.text},
         ]
     )
-    # print(response)
 
     request_tokens = response["usage"]["total_tokens"]  # same: response.usage.total_tokens
-    session_tokens += request_tokens
-    request_number += 1
+    static.session_tokens += request_tokens
+    static.request_number += 1
 
     data["tokens"] += request_tokens
     data["requests"] += 1
@@ -71,19 +59,19 @@ def handle_message(message):
     with open(filename, "w") as f:
         json.dump(data, f)
 
-    request_price = request_tokens * price_cents
+    request_price = request_tokens * static.price_cents
     # формируем лог работы для юзера
     user_log = f"\n\n\nТокены: {request_tokens} за ¢{round(request_price, 4)}. " \
-               f"\nОбщая стоимость сессии: ¢{round(session_tokens * price_cents, 4)}"
+               f"\nОбщая стоимость сессии: ¢{round(static.session_tokens * static.price_cents, 4)}"
 
     # Send the response back to the user
     bot.send_message(message.chat.id, response.choices[0].message.content + user_log)
 
     # формируем лог работы для админа
-    admin_log = (f"Запрос {request_number}: {request_tokens} токенов за ¢{round(request_price, 4)},"
-                 f" всего {session_tokens} за ¢{round(session_tokens * price_cents, 4)},"
+    admin_log = (f"Запрос {static.request_number}: {request_tokens} токенов за ¢{round(request_price, 4)},"
+                 f" всего {static.session_tokens} за ¢{round(static.session_tokens * static.price_cents, 4)},"
                  f" {message.chat.first_name} {message.chat.last_name} @{message.chat.username} {message.chat.id}"
-                 f"\n{data}, ¢{round(data['tokens']*price_cents, 4)}")
+                 f"\n{data}, ¢{round(data['tokens']*static.price_cents, 4)}")
 
     # пишем лог работы в консоль
     print(admin_log)
@@ -93,5 +81,7 @@ def handle_message(message):
         bot.send_message(admin_chat_id, admin_log)
 
 
-# Start the bot
-bot.polling()
+if __name__ == "__main__":
+    # Start the bot
+    print("Started")
+    bot.infinity_polling()
---
+++ static.py
@@ -0,0 +1,5 @@
+prompt = "You are a helpful assistant."  # prompt = "You are Marv - a sarcastic reluctant assistant."
+session_tokens = 0
+request_number = 0
+price_1k = 0.002
+price_cents = price_1k / 10
