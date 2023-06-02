98a506d9d8f5a71414aed419366573321e9704ea
kxkw <130280061+kxkw@users.noreply.github.com>
2023-06-03 03:31:03 +0600

    Update the initial admin data in `data.json`
    Add `name` and `username` fields

--- main.py
+++ main.py
@@ -121,7 +121,8 @@ if os.path.isfile(DATAFILE):
         data[int(key)] = data.pop(key)
 else:
     data = {"global": {"requests": 0, "tokens": 0},
-            ADMIN_ID: {"requests": 0, "tokens": 0, "balance": 777777, "lastdate": "01-05-2023 00:00:00"}}
+            ADMIN_ID: {"requests": 0, "tokens": 0, "balance": 777777,
+                       "name": "АДМИН", "username": "@admin", "lastdate": "01-05-2023 00:00:00"}}
     # Create the file with default values
     update_json_file(data)
 
